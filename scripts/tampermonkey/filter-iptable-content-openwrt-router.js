f49734155c2cd61e3ec792d696d373789a6155d9
udoline <udo@lipsia.de>
2021-11-28 02:06:28 +0100

    add new feature Filter this content by selected ipaddress
    If one element selected then only the content from this IP-address is shown.
    
    new version 0.0.3

--- scripts/tampermonkey/filter-iptable-content-openwrt-router.js
+++ scripts/tampermonkey/filter-iptable-content-openwrt-router.js
@@ -3,7 +3,7 @@
 // @namespace    http://openwrt.org/docs/guide-user/luci/start
 // @updateURL    https://raw.githubusercontent.com/udoline/filter-conns-openwrt-router/main/scripts/tampermonkey/filter-iptable-content-openwrt-router.js
 // @downloadURL  https://raw.githubusercontent.com/udoline/filter-conns-openwrt-router/main/scripts/tampermonkey/filter-iptable-content-openwrt-router.js
-// @version      0.0.2
+// @version      0.0.3
 // @description  Filter some network traffic content over the firewall by ip-address or dns-name on your OpenWrt brick/router is running OpenWrt 21.02.0-rc3
 // @author       udoline
 // @match        https://192.168.1.1/cgi-bin/luci/admin/status/iptables
@@ -16,11 +16,27 @@
 (function() {
     'use strict';
 
+    const map = new Map();
+    function sortMapByKey(map) {
+        let keys = [];
+        for ( const [key, value] of map.entries() ){
+            keys.push( key );
+        }
+
+        keys.sort();
+        let sortedMap = new Map();
+        for (let i = 0; i < keys.length; i++) {
+            sortedMap.set( keys[i], map.get(keys[i]) ) ;
+        }
+
+        return sortedMap;
+    }
+
     function jquerify( jquerified ){
 		if(!(window.JQuery && window.Jquery.fn.jquery === '1')){
 			let s = document.createElement('script');
 			let m = "jquery/3.6.0";
-			s.setAttribute('src','https://ajax.googleapis.com/ajax/libs/" + m + "/jquery.min.js');
+			s.setAttribute('src','https://ajax.googleapis.com/ajax/libs/' + m + '/jquery.min.js');
 			s.setAttribute('type','text/javascript');
 			document.getElementsByTagName('head')[0].appendChild(s);
 			jquerified = true;
@@ -39,12 +55,40 @@
 
 					$( "button:contains('Hide empty chains')" ).click();
 
+                    // delete useless data
 					$( 'td[data-title="Pkts."]:contains("0")' ).each(function() {
 						// remove empty content
 						if( $(this).text().match(/^\s*0\s*$/) ) {
 							$(this).closest("tr").remove();
 						}
 					});
+                    // collect data
+                    $( 'td[data-title="Source"]:not(:contains("0.0.0.0/0"))' ).each(function( i ) {
+                        let dump = $(this).text();
+                        if( map.get(dump) !== 's' )
+                            map.set(dump, 's');
+					});
+                    $( 'td[data-title="Destination"]:not(:contains("0.0.0.0/0"))' ).each(function( i ) {
+                        let dump = $(this).text();
+                        if( map.get(dump) !== 'd' )
+                            map.set(dump, 'd');
+					});
+                    // present collected data
+                    if( map.size > 0 && ! $('select[name="ip_addr_src"]').length ) {
+                        let html = '<span style="padding-right:2;">\n<select name="ip_addr_src" style="width:121;" ';
+                        html += ' title="Filter this content by selected ipaddress ...">\n'
+                        html += '<option title="Remove selected filter ..." id="empty" value=""></option>\n';
+                        for ( const [key, value] of sortMapByKey(map).entries() ) {
+                            html += '<option title="Use this as filter ..." value="' + key + '">' + key + '</option>\n';
+                        }
+                        html += '</select>\n</span>\n';
+                        $( html ).insertBefore( $( 'button[data-hide-empty]' ) );
+                    }
+                    // use picked ipaddress as filter
+                    const filterIpAddr = $( 'select[name="ip_addr_src"] option:selected' ).text();
+                    if( !!filterIpAddr && filterIpAddr !== "" ) {
+                        $( 'td[data-title="Source"]:not(:contains("' + filterIpAddr + '"))' ).closest("tr").remove();
+                    }
 				}
 			}, false);
 			open.apply(this, arguments);
