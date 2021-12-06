6f442ff1f9d4fad6dbe5f9280e980a61551bbf3a
udoline <udo@lipsia.de>
2021-12-06 13:06:39 +0100

    Bug fix  in ressource URL
    The new version is 0.0.5 .

--- scripts/tampermonkey/filter-conns-openwrt-router.js
+++ scripts/tampermonkey/filter-conns-openwrt-router.js
@@ -3,7 +3,7 @@
 // @namespace    https://openwrt.org/docs/guide-user/luci/start
 // @updateURL    https://raw.githubusercontent.com/udoline/filter-conns-openwrt-router/main/scripts/tampermonkey/filter-conns-openwrt-router.js
 // @downloadURL  https://raw.githubusercontent.com/udoline/filter-conns-openwrt-router/main/scripts/tampermonkey/filter-conns-openwrt-router.js
-// @version      0.0.4
+// @version      0.0.5
 // @description  Filter some network traffic by ip-address or dns-name on your OpenWrt brick/router is running OpenWrt 21.02.0-rc3
 // @author       udoline
 // @match        http://192.168.1.1/cgi-bin/luci/admin/status/realtime/connections
@@ -19,7 +19,7 @@ function jquerify( jquerified ){
     if(!(window.JQuery && window.Jquery.fn.jquery === '1')){
         let s = document.createElement('script');
         let m = "jquery/3.6.0";
-        s.setAttribute('src','https://ajax.googleapis.com/ajax/libs/" + m + "/jquery.min.js');
+        s.setAttribute('src','https://ajax.googleapis.com/ajax/libs/' + m + '/jquery.min.js');
         s.setAttribute('type','text/javascript');
         document.getElementsByTagName('head')[0].appendChild(s);
         jquerified = true;
